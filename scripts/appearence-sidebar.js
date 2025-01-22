adcbd0147bae98ac65af19c3244994a6fdb45ef2
Maxelone <maxelone@yandex.ru>
2025-01-23 03:47:57 +0400

    Load icons in appearence sidebar
    Getting always error because there is always no 12 png images but im still loading them...

--- scripts/appearence-sidebar.js
+++ scripts/appearence-sidebar.js
@@ -1,5 +1,3 @@
-// TODO: Load pics from ./images to sidebarPickAppearence.content.appearence.cell as img.icon
-
 const sidebarPickCharacter = document.querySelector(".sidebar.pick-character");
 const sidebarPickAppearence = document.querySelector(
   ".sidebar.pick-appearence",
@@ -18,4 +16,49 @@ if (sidebarPickAppearence) {
     sidebarPickAppearence.classList.remove("active");
     character.classList.remove("appearence");
   });
+
+  const cells = sidebarPickAppearence.querySelectorAll(".appearence .cell");
+
+  const appearences = sidebarPickAppearence.querySelectorAll(".icon_container");
+  appearences.forEach((appearence) => {
+    appearence.addEventListener("click", () => {
+      // Удалить у всех .icon_container класс .active
+      appearences.forEach((appearence) =>
+        appearence.classList.remove("active"),
+      );
+      // Удалить у всех ячеек существовавшие изображения
+      cells.forEach((cell) => {
+        cell.innerHTML = "";
+      });
+
+      // Присвоить текущему .icon_container класс active
+      appearence.classList.add = "active";
+      const activeAppearence = appearence.classList[1];
+      uploadCells(activeAppearence);
+    });
+  });
+
+  // Подгрузка внешности в ячейки от активной иконки
+  const uploadCells = (activeAppearence) => {
+    for (let i = 1; i <= cells.length; i++) {
+      const image = document.createElement("img");
+      image.className = "icon";
+      image.src = `../images/appearence/${activeAppearence}/${i}.png`;
+
+      // Костыль без бекенда
+      // Изображения грузятся 1-12.png, даже если их нет
+      // TODO: изменить
+      image.onerror = () => {
+        image.remove();
+        console.clear();
+      };
+
+      cells[i - 1].appendChild(image);
+    }
+    const image = document.createElement("img");
+  };
+
+  // По умолчанию первая иконка - активна
+  appearences[0].classList.add("active");
+  uploadCells(appearences[0].classList[1]);
 }
