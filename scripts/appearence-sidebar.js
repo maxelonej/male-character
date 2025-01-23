9a0ea18fe48f1e711311f0ac43feeeeec418a8cc
Maxelone <maxelone@yandex.ru>
2025-01-23 08:35:44 +0400

    feat: Add ability to change character's clothes

--- scripts/appearence-sidebar.js
+++ scripts/appearence-sidebar.js
@@ -17,10 +17,12 @@ if (sidebarPickAppearence) {
     character.classList.remove("appearence");
   });
 
-  const cells = sidebarPickAppearence.querySelectorAll(".appearence .cell");
+  const cellsWrapper = sidebarPickAppearence.querySelector(".appearence");
+  const cells = cellsWrapper.querySelectorAll(".cell");
 
   const appearences = sidebarPickAppearence.querySelectorAll(".icon_container");
   appearences.forEach((appearence) => {
+    // При клике на иконку: (парик/одежда/тд)
     appearence.addEventListener("click", () => {
       // Удалить у всех .icon_container класс .active
       appearences.forEach((appearence) =>
@@ -32,7 +34,8 @@ if (sidebarPickAppearence) {
       });
 
       // Присвоить текущему .icon_container класс active
-      appearence.classList.add = "active";
+      appearence.classList.add("active");
+      // Узнать что это за внешность (face/wig/pants/etc)
       const activeAppearence = appearence.classList[1];
       uploadCells(activeAppearence);
     });
@@ -55,10 +58,51 @@ if (sidebarPickAppearence) {
 
       cells[i - 1].appendChild(image);
     }
-    const image = document.createElement("img");
   };
 
   // По умолчанию первая иконка - активна
   appearences[0].classList.add("active");
   uploadCells(appearences[0].classList[1]);
+
+  cells.forEach((cell) => {
+    cell.addEventListener("click", () => {
+      // TODO: добавить прозрачную иконку и обозначить, что не искать такую же и не добавлять её на перса
+
+      // .container .character > img.wig + img.face + img.shirt + img.pants
+      // .pick-character .character.mini > img.wig + img.face + img.shirt + img.pants
+      // брать путь дочернего элемента - изображения.icon
+      const icon = cell.querySelector(".icon");
+      const iconUrl = icon.getAttribute("src"); // images/appearence/face/1.png
+      const currentAppearenceUrl = iconUrl.substring(18, iconUrl.length); // face/1.png
+      const appearenceFromUrl = currentAppearenceUrl.split("/")[0]; // face
+
+      // Путь одежды для персонажа, исходя из пути ячейки
+      const outfitUrl = `images/outfit/${currentAppearenceUrl}`;
+      // применить найденное на персонажа
+      const character = document.querySelector(".container .character");
+
+      switch (appearenceFromUrl) {
+        case "face":
+          const face = character.querySelector(".face");
+          face.src = outfitUrl;
+          break;
+        case "wig":
+          const wig = character.querySelector(".wig");
+          wig.src = outfitUrl;
+          break;
+        case "shirt":
+          const shirt = character.querySelector(".shirt");
+          shirt.src = outfitUrl;
+          break;
+        case "pants":
+          const pants = character.querySelector(".pants");
+          pants.src = outfitUrl;
+          break;
+        // default:
+        //   console.log(`${appearenceFromUrl} не существует`);
+      }
+      // TODO: by click on new session: add img.body src="images/body/human.png"
+      // TODO: different session by .cell.1 .cell.2 ~ .cell.4 by localStorage smhw
+    });
+  });
 }
